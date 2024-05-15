3a149e3fa3e59bf43878b8a1981b2631851d56f5
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-15 17:42:09 +0200

    Print colors demo

--- 02-print.sh
+++ 02-print.sh
@@ -7,5 +7,5 @@ echo -e "\e[33mHello World\e[0m"
 echo -e "\e[34mHello World\e[0m"
 echo -e "\e[35mHello World\e[0m"
 echo -e "\e[36mHello World\e[0m"
-echo -e "\e[31mHello World"
+echo -e "\e[31mHello World\e[0m"
 echo Hi colour\e[0m
