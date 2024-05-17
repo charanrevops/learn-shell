209d7de3db8dbbae401cc8980a8658c60c493871
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-17 16:03:34 +0200

    accesing special variables

--- 03-variable.sh
+++ 03-variable.sh
@@ -14,6 +14,6 @@ echo all arguments - $*
 echo number of arguments - $#
 
 
-lastname_firstname=kotha' 'charan' 'reddy
+lastname_firstname=kotha' 'charan' 'reddy' '$&
 
 echo FullName is $lastname_firstname
