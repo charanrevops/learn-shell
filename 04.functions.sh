c2980f84a237ac436e89336fc4b493694be70592
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-17 23:11:32 +0200

    accesing special variables

--- 04.functions.sh
+++ 04.functions.sh
@@ -4,5 +4,10 @@ print_hello ()
    echo hello world
     echo ' ' hello world
       echo   hello'    '   world
+      echo x from main program is $x
+      y=20
 }
+
+x=20
 print_hello
+echo y from function is $y
