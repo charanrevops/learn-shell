5766abc5e5c28f4738388e451bfee3884261e057
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-17 15:39:45 +0200

    Special variables demo

--- 03-variable.sh
+++ 03-variable.sh
@@ -8,5 +8,7 @@ echo exit status-$?
 echo script name - $0
 echo first argument - $1
 echo second argument - $2
+echo first argument - $3
+echo second argument - $1
 echo all arguments - $*
 echo number of arguments - $#
