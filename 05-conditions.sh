1fec1446f56220687facc959f80243d433b78aa1
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-24 12:07:50 +0200

    Conditions

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -22,8 +22,9 @@ if  [ -z "$1" ]; then
     echo No input is provided
 elif [ -z "$2" ];then
   echo first input is provided and the first input is $1
+  echo second input is provided and the second input is $2
   echo No 2nd input is provided
 else
-  echo Input is provided
+  echo First and seconfd inputs are provided
 fi
 
