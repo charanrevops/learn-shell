665254ee908a8e68c5bccb5e000106c329a6657e
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-24 11:54:43 +0200

    Adding common script for keeping code Dry

---
+++ 05-conditions.sh
@@ -0,0 +1,20 @@
+a=10
+
+if [$a -gt 5] ; then
+  echo $a is greater than 5
+fi
+
+
+
+b=80
+
+if [$b -gt 75] ; then
+  echo pass-disctinction
+elif [$b -gt 60]; then
+  echo Pass-First class
+else
+  echo Fail / Second class
+fi
+
+
+
