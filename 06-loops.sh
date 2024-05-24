ffed31d67c03cc3ded789083c573594a6911a05f
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-24 17:28:57 +0200

    Adding Backend.sh with standard options.

--- 06-loops.sh
+++ 06-loops.sh
@@ -7,7 +7,7 @@ while [ $i -gt 0 ];do
 done
 
 #for
-for course in devops aws azure; do
-  echo welcome to $course training
+for fruits in devops aws azure; do
+  echo welcome to $fruits training
   sleep 1
 done
