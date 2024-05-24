72b1f77f8b472e170042f7f2a0daf514466e78ee
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-24 11:57:42 +0200

    Conditions

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -8,9 +8,9 @@ fi
 
 b=80
 
-if [$b -gt 75] ; then
+if [ $b -gt 75 ] ; then
   echo pass-disctinction
-elif [$b -gt 60]; then
+elif [ $b -gt 60 ]; then
   echo Pass-First class
 else
   echo Fail / Second class
